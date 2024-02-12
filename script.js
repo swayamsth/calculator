30589a8daeabfa8bb50c4765860ce8ebf7c8e16f
Swayam Shrestha <swayams24@gmail.com>
2024-02-13 01:33:20 +1100

    Create variables and func to operate based on the operation given

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1 / num2;
 }
+
+let firstNum, lastNum, operator;
+
+function operate(firstNum,lastNum,operator){
+    switch(operator){
+        case "+":
+            result = add(firstNum,lastNum);
+            break;
+        case "-":
+            result = subtract(firstNum,lastNum);
+            break;
+        case "*":
+            result = multiply(firstNum,lastNum);
+            break;
+        case "/":
+            result = divide(firstNum,lastNum);
+            break;
+    }
+    return result;
+}
