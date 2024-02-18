be963e1d63242414be76fa5a66b155d0e8931bee
Swayam Shrestha <swayams24@gmail.com>
2024-02-18 23:17:20 +1100

    Clear Buttons working kinda

--- script.js
+++ script.js
@@ -33,3 +33,37 @@ function operate(firstNum,lastNum,operator){
     }
     return result;
 }
+
+const numbers = document.querySelectorAll(".numbers");
+const displayResult = document.querySelector(".display");
+const evaluate = document.getElementById("evaluate");
+
+//Clear Buttons
+
+const clearBtns = document.querySelectorAll(".clearBtn button");
+
+clearBtns.forEach(item => {
+    item.addEventListener('click', (e) => {
+        let event = e.target.name;
+        if (event === "reset"){
+            displayResult.textContent = "0";
+        } else if (event === "backspace"){
+            displayResult.textContent = displayResult.textContent.slice(0,-1);
+        }
+    })
+})
+
+// console.log(clearBtns);
+// console.log(numbers);
+// numbers.forEach(item => {
+//     item.addEventListener('click', (e) => {
+//         const num = e.target.textContent; 
+//         appendToDisplay(num);
+//         console.log(e.target.textContent);
+//     })
+// })
+
+function appendToDisplay(num){
+    displayResult.textContent += num;
+}
+
